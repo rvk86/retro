9098d382a9a9e9cbdff3561144c741e8cdf84519
Ralph <ralph@stamkracht.com>
2017-04-21 01:12:12 +0200

    Added Bootstrap-sass and geosuggest

--- app/src/components/map/Map.js
+++ app/src/components/map/Map.js
@@ -1,40 +1,64 @@
-import React, { PropTypes, Component } from 'react';
+import React, {
+  PropTypes,
+  Component
+} from 'react';
 import ReactDOM from 'react-dom';
 import GoogleMapReact from 'google-map-react';
+import Geosuggest from 'react-geosuggest';
 
 import Config from '../../config';
 import './Map.css';
 
 class Map extends Component {
   state = {
-    defaultCenter: {lat: 59.95, lng: 30.33},
+    defaultCenter: {
+      lat: 59.95,
+      lng: 30.33
+    },
     defaultZoom: 11,
     center: null,
     language: 'nl'
   }
-  
+
   static propTypes = {
     onChange: PropTypes.func
   }
-  
+
   componentWillMount() {
-    if(navigator.geolocation) {
+    if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((res) => {
-        this.setState({center: {lat: res.coords.latitude, lng: res.coords.longitude}});
+        this.setState({
+          center: {
+            lat: res.coords.latitude,
+            lng: res.coords.longitude
+          }
+        });
       });
     } else {
-      this.setState({center: {lat: 52.3557194, lng: 4.8889233}});
+      this.setState({
+        center: {
+          lat: 52.3557194,
+          lng: 4.8889233
+        }
+      });
     }
   }
-  
+
   componentDidMount() {
     let el = ReactDOM.findDOMNode(this);
     el.style.height = `${el.clientWidth}px`;
   }
 
+  setCenter = (suggest) => {
+    this.setState({
+      center: suggest.location
+    });
+    console.log(suggest)
+  }
+
   render() {
     let overlay;
-    if(this.props.children) {
+    if (this.props.children) {
       overlay = (
         <div className="MapOverlay">
           {this.props.children}
@@ -43,6 +67,11 @@ class Map extends Component {
     }
     return (
       <div className="Map">
+        <Geosuggest className="form-group" 
+                    inputClassName="form-control"
+                    suggestsClassName="btn-group-vertical"
+                    suggestItemClassName="btn btn-default"
+                    onSuggestSelect={this.setCenter}/>
         <GoogleMapReact
             bootstrapURLKeys={{
               key: Config.googleMapsApiKey
