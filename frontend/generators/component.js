5a98e3e8a153e826cebe2d965d2fb2c8ff8571f9
Ralph <ralph@stamkracht.com>
2017-05-10 12:58:45 +0200

    Added component generator

---
+++ frontend/generators/component.js
@@ -0,0 +1,44 @@
+var fs = require("fs-extra");
+
+function copyComponent() {
+  let camelName = process.argv[3].charAt(0).toLowerCase() + process.argv[3].slice(1);
+  let capitalName = process.argv[3].charAt(0).toUpperCase() + process.argv[3].slice(1);
+
+  let source = `${__dirname}/component`;
+  let target = `${__dirname}/../src/${process.argv[2]}/${camelName}`;
+
+  // Create new directory
+  if(fs.existsSync(target)) return console.log('component with this name already exists.');
+  fs.mkdirSync(target);
+
+  // Find and replace **NAME** with component name
+  fs.readdir(source, function(err, files) {
+    if( err ) return console.error(err);
+    files.forEach(function(file) {
+      fs.readFile(`${source}/${file}`, 'utf8', function (err,data) {
+        if (err) return console.log(err);
+
+        let result = data.replace(/\*\*NAME\*\*/g, capitalName);
+        let fileName = file.replace(/Component/, capitalName);
+        fs.writeFile(`${target}/${fileName}.js`, result, 'utf8');
+      });
+    });
+
+  });
+}
+
+if (require.main === module) {
+  if(process.argv.length < 4) {
+    console.log(
+      `
+To generate a new component run this script as follows:
+node component.js [DESTINATION DIRECTORY] [COMPONENT NAME]
+where DESTINATION DIRECTORY is a folder inside the src directory (mostly components or containers)
+and COMPONENT NAME is the name of the component camelcased.
+      `
+    )
+    return;
+  }
+
+  copyComponent();
+}
---
+++ frontend/generators/component/Component.js
@@ -0,0 +1,18 @@
+import React, { PropTypes, Component } from 'react';
+
+import './**NAME**.css';
+
+class **NAME** extends Component {
+  static propTypes = {
+    // prop: PropTypes.func
+  }
+
+  render() {
+    return (
+      <div className="**NAME**">
+      </div>
+    );
+  }
+}
+
+export default **NAME**;
